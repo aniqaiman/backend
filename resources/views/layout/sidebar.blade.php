30e43991ffc678699009c04773f760349445bb45
User-PC\User <aniq.aiman@me-tech.com.my>
2018-03-12 20:17:02 +0800

    sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -39,52 +39,91 @@
           </a>
         </li>
 
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-usd"></i>
-            <span class="text-info">Supplier / Buyer</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a class="text-info" href="{{route('seller')}}"><i class="fa fa-level-down"></i>Supplier Management</a></li>
-            <li><a class="text-info" href="{{route('buyer')}}"><i class="fa fa-level-down"></i>Buyer Management</a></li>
-            <li><a class="text-info" href="{{route('group')}}"><i class="fa fa-level-down"></i>Group Management</a></li>
-          </ul>
-        </li>
-
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-product-hunt"></i>
-            <span class="text-info">Product Management</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a class="text-info" href="{{route('fruit')}}"><i class="fa fa-level-down"></i>Fruit Management</a></li>
-            <li><a class="text-info" href="{{route('vege')}}"><i class="fa fa-level-down"></i>Vegetable Management</a></li>
-          </ul>
-        </li>
-
-        <li class="treeview">
+        <!-- <li class="treeview">
           <a href="{{route('driver')}}">
             <i class="fa fa-truck"></i> <span class="text-info">Driver Management</span>
           </a>
-        </li>
+        </li> -->
+
+        <!-- <li class="treeview">
+          <a href="{{route('order')}}">
+            <i class="fa fa-shopping-cart"></i> <span class="text-info">Order Management</span>
+          </a>
+        </li> -->
 
         <li class="treeview">
           <a href="{{route('order')}}">
-            <i class="fa fa-shopping-cart"></i> <span class="text-info">Order Management</span>
+            <i class="fa fa-shopping-cart"></i> <span class="text-info">Order Tracking</span>
+          </a>
+        </li> 
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-comments"></i> <span class="text-info">Feedback Trail</span>
+          </a>
+        </li> 
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-archive"></i>
+            <span class="text-info">Stock Count</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a class="text-info" href="{{route('fruit')}}"><i class="fa fa-level-down"></i>Fruit Stock</a></li>
+            <li><a class="text-info" href="{{route('vege')}}"><i class="fa fa-level-down"></i>Vegetable Stock</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-user"></i> <span class="text-info">SKU Member ID</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dollar"></i> <span class="text-info">Price Setting Page</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-calendar"></i> <span class="text-info">Earning Summary</span>
+          </a>
+        </li> 
+
+        <li class="treeview">
+          <a href="{{route('driver')}}">
+            <i class="fa fa-truck"></i> <span class="text-info">Lorry Driver</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="{{route('seller')}}">
+            <i class="fa fa-user"></i> <span class="text-info">Supplier Listing</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="{{route('buyer')}}">
+            <i class="fa fa-user-plus"></i> <span class="text-info">Buyer Listing</span>
           </a>
         </li>
 
         <li class="treeview">
           <a href="{{route('users')}}">
-            <i class="fa fa-users"></i> <span class="text-info">User Management</span>
+            <i class="fa fa-male"></i> <span class="text-info">User Management</span>
           </a>
         </li> 
+
+        <li class="treeview">
+          <a href="{{route('group')}}">
+            <i class="fa fa-users"></i> <span class="text-info">Group Management</span>
+          </a>
+        </li>
         
         </ul>
     </section>
