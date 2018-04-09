cba0bc266ca63589d7c90a7d017770197cf6a30b
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-09 15:30:08 +0800

    update lagi untuk test

--- routes/web.php
+++ routes/web.php
@@ -146,4 +146,3 @@ Route::get('/api/orders', 'Api\OrderController@getOrders');
 Route::get('/api/cart/{user_id}', ['as' => 'getCartItems', 'uses' => 'Api\CartController@getCartItems']);
 Route::get('/api/cart/{user_id}/total', ['as' => 'getCartItems', 'uses' => 'Api\CartController@getTotalCartItems']);
 Route::post('/api/cart', ['as' => 'postCartItem', 'uses' => 'Api\CartController@postCartItem']);
-Route::delete('/api/cart/{user_id}-{product_id}', ['as' => 'deleteCartItem', 'uses' => 'Api\CartController@deleteCartItem']);
