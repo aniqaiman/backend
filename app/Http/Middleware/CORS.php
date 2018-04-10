473da74288b414f929408eb3e5e53235706f92ae
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-10 21:10:10 +0800

    restore cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -2,9 +2,13 @@
 # File: app\Http\Middleware\CORS.php
 # Create file with below code in above location. And at the end of the file there are other instructions also. 
 # Please check. 
+
 namespace App\Http\Middleware;
+
 use Closure;
+
 class CORS {
+
     /**
      * Handle an incoming request.
      *
@@ -12,15 +16,16 @@ class CORS {
      * @param  \Closure  $next
      * @return mixed
      */
-    
     public function handle($request, Closure $next) {
 //        return $next($request);
         header("Access-Control-Allow-Origin: *");
+
         // ALLOW OPTIONS METHOD
         $headers = [
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
         ];
+
         if ($request->getMethod() == "OPTIONS") {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return \Response::make('OK', 200, $headers);
@@ -28,7 +33,9 @@ class CORS {
 
         $response = $next($request);
         foreach ($headers as $key => $value)
-           $response->headers->set($key, $value); 
+            $response->header($key, $value);
         return $response;
     }
 }
+
+
