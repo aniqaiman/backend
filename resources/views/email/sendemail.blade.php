94f969fd6e16d75e8cdf4b2566fd344287d7d999
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-20 23:29:31 +0800

    proper template

--- resources/views/email/sendemail.blade.php
+++ resources/views/email/sendemail.blade.php
@@ -3,63 +3,64 @@
         font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
 
-    h1 {
-        margin-top: 0;
+    #wrapper {
+        margin: 0 auto;
+        background-color: #343a40;
+        padding: 25px;
+        text-align: center;
+        color: white;
+        max-width: 670px;
     }
 
-    a {
+    #wrapper #logo {
+        width: 190px;
+        margin-top: 5px;
+        margin-bottom: 15px;
+    }
+
+    #wrapper #inner {
+        background-color: white;
+        border-radius: 3px;
+        padding: 30px 30px 20px;
+        text-align: left;
+        color: black;
+        margin-bottom: 5px;
+    }
+
+    #wrapper #inner h1 {
+        margin-top: 0;
+        text-align: center;
+    }
+
+    #wrapper #inner a {
         text-decoration: none;
     }
 
-    .btnActivate {
-        -moz-box-shadow: 0px 10px 14px -7px #3e7327;
-        -webkit-box-shadow: 0px 10px 14px -7px #3e7327;
-        box-shadow: 0px 10px 14px -7px #3e7327;
-		background-color: #77b55a;
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #77b55a), color-stop(1, #72b352));
-        background: -moz-linear-gradient(top, #77b55a 5%, #72b352 100%);
-        background: -webkit-linear-gradient(top, #77b55a 5%, #72b352 100%);
-        background: -o-linear-gradient(top, #77b55a 5%, #72b352 100%);
-        background: -ms-linear-gradient(top, #77b55a 5%, #72b352 100%);
-        background: linear-gradient(to bottom, #77b55a 5%, #72b352 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#77b55a', endColorstr='#72b352', GradientType=0);
+    #wrapper #inner #btnActivate {
+        text-align: center;
+    }
+
+    #wrapper #inner #btnActivate a {
         background-color: #77b55a;
-        -moz-border-radius: 4px;
-        -webkit-border-radius: 4px;
-        border-radius: 4px;
+        -moz-border-radius: 3px;
+        -webkit-border-radius: 3px;
+        border-radius: 3px;
         border: 1px solid #4b8f29;
         display: inline-block;
         cursor: pointer;
-        color: #ffffff;
+        color: #fff;
         font-size: 1em;
         padding: 10px 15px;
         text-decoration: none;
-        text-shadow: 0px 1px 0px #5b8a3c;
-    }
-
-    .btnActivate:hover {
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #72b352), color-stop(1, #77b55a));
-        background: -moz-linear-gradient(top, #72b352 5%, #77b55a 100%);
-        background: -webkit-linear-gradient(top, #72b352 5%, #77b55a 100%);
-        background: -o-linear-gradient(top, #72b352 5%, #77b55a 100%);
-        background: -ms-linear-gradient(top, #72b352 5%, #77b55a 100%);
-        background: linear-gradient(to bottom, #72b352 5%, #77b55a 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#72b352', endColorstr='#77b55a', GradientType=0);
-        background-color: #72b352;
-    }
-
-    .btnActivate:active {
-        position: relative;
-        top: 1px;
     }
 </style>
 
 <body>
-    <div style="margin: 0 auto; background-color: #343a40; padding: 25px; text-align: center; color: white; max-width: 670px;">
-        <img src="https://ninjavan.herokuapp.com/img/logo/logo-text.png" alt="Food Rico Logo" style="width: 190px; margin-top: 5px; margin-bottom: 15px;">
+    <div id="wrapper">
+        <img src="https://ninjavan.herokuapp.com/img/logo/logo-text.png" alt="Food Rico Logo" id="logo">
 
-        <div style="background-color: white; border-radius: 3px; padding: 30px 30px 20px; text-align: left; color: black; margin-bottom: 5px;">
-            <h1 style="text-align: center;">
+        <div id="inner">
+            <h1>
                 The FoodRico {{ $user->group->name }}
                 <br />
                 <sup>Welcome Onboard!</sup>
@@ -72,8 +73,8 @@
                 <br /> To continue, please confirm your email by clicking the button below:
             </p>
 
-            <div style="text-align: center;">
-                <a href="#" class="btnActivate">Activate My E-Mail</a>
+            <div id="btnActivate">
+                <a href="#">Activate My E-Mail</a>
             </div>
 
             <p>
