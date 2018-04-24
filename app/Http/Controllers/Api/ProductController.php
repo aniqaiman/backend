27bac3e60588adde69c9464692ecb211940aeff2
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-24 20:13:07 +0800

    Revert "Updated on route and product controller"
    This reverts commit 00ff38ad7dbdae8e37c5626926b917417a743edf.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -13,30 +13,66 @@ class ProductController extends Controller
 {
     public function getProducts(Request $request)
     {
-        return response()->json(
-            Product::get()
-        );
+        $products = Product::orderBy("id", "desc")->get();
+
+        $productArray = [];
+
+        foreach ($products as $product) {
+            $newproduct["id"] = $product->id;
+            $newproduct["name"] = $product->name;
+            $newproduct["desc"] = $product->desc;
+            $newproduct["short_desc"] = $product->short_desc;
+            $newproduct["quantity"] = $product->quantity;
+            $newproduct["image"] = $product->image;
+            $newproduct["created_at"] = $product->created_at;
+            $newproduct["updated_at"] = $product->updated_at;
+            $newproduct["category"] = $product->category;
+
+            array_push($productArray, $newproduct);
+        }
+
+        return response()->json(["data" => $productArray, "status" => "ok"]);
     }
 
     public function getProductById($product_id, Request $request)
     {
-        return response()->json(
-            Product::find($product_id)
-        );
+        $product = Product::where("id", $product_id)->first();
+        $prices = Price::where("product_id", $product_id)
+            ->orderBy("created_at", "desc")
+            ->take(2)
+            ->get();
+
+        $newproduct["id"] = $product->id;
+        $newproduct["name"] = $product->name;
+        $newproduct["desc"] = $product->desc;
+        $newproduct["short_desc"] = $product->short_desc;
+        $newproduct["quantity"] = $product->quantity;
+        $newproduct["image"] = $product->image;
+        $newproduct["category"] = $product->category;
+        $newproduct["priceA"] = $prices[0]->product_price;
+        $newproduct["priceB"] = $prices[0]->product_price2;
+        $newproduct["priceC"] = $prices[0]->product_price3;
+        $newproduct["priceADiff"] = sizeof($prices) > 1 ? round(($prices[0]->product_price - $prices[1]->product_price) / $prices[1]->product_price, 2) : 0;
+        $newproduct["priceBDiff"] = sizeof($prices) > 1 ? round(($prices[0]->product_price2 - $prices[1]->product_price2) / $prices[1]->product_price2, 2) : 0;
+        $newproduct["priceCDiff"] = sizeof($prices) > 1 ? round(($prices[0]->product_price3 - $prices[1]->product_price3) / $prices[1]->product_price3, 2) : 0;
+
+        return response()->json(["data" => $newproduct, "status" => "ok"]);
     }
 
-    public function getProductsByCategory($category_id, Request $request)
+    public function getFruits(Request $request)
     {
-        return response()->json(
-            Product::where('category_id', $category_id)->get()
-        );
+        $fruits = Product::where("category_id", 1)
+            ->paginate(15);
+
+        return response($fruits);
     }
 
-    public function getProductsByCategoryWithPage($category_id, Request $request)
+    public function getVegetables()
     {
-        return response()->json(
-            Product::where('category_id', $category_id)->paginate()
-        );
+        $vegetables = Product::where("category_id", 11)
+            ->paginate(15);
+
+        return response($vegetables);
     }
 
     public function getPrices(Request $request)
