6db0ec9e9b9f1c0c77151f871f0ca079fcc2b9eb
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-25 23:13:26 +0800

    update button show

--- resources/views/order/trackings.blade.php
+++ resources/views/order/trackings.blade.php
@@ -80,15 +80,11 @@
                 </td>
                 <td class="text-nowrap">
                   <div class="btn-group-vertical btn-group-sm" role="group">
-                    @if ($order->status === 4)
-                    <button class="btn btn-success active" disabled>Paid</button>
-                    @else
+                    @if ($order->status !== 4)
                     <button class="btn btn-success" data-id="{{ $order->id }}" data-status="4" onclick="updateStatus(this)">Paid</button>
                     @endif
                     
-                    @if ($order->status === 3)
-                    <button class="btn btn-danger active" disabled>Pending</button>
-                    @else
+                    @if ($order->status !== 3)
                     <button class="btn btn-danger" data-id="{{ $order->id }}" data-status="3" onclick="updateStatus(this)">Pending</button>
                     @endif
                   </div>
