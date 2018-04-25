a62b9c629adc58597e303d5bc7ae30bf65127915
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-25 23:14:59 +0800

    revert active

--- resources/views/order/trackings.blade.php
+++ resources/views/order/trackings.blade.php
@@ -80,11 +80,15 @@
                 </td>
                 <td class="text-nowrap">
                   <div class="btn-group-vertical btn-group-sm" role="group">
-                    @if ($order->status !== 4)
+                    @if ($order->status === 4)
+                    <button class="btn btn-success" disabled>Paid</button>
+                    @else
                     <button class="btn btn-success" data-id="{{ $order->id }}" data-status="4" onclick="updateStatus(this)">Paid</button>
                     @endif
                     
-                    @if ($order->status !== 3)
+                    @if ($order->status === 3)
+                    <button class="btn btn-danger" disabled>Pending</button>
+                    @else
                     <button class="btn btn-danger" data-id="{{ $order->id }}" data-status="3" onclick="updateStatus(this)">Pending</button>
                     @endif
                   </div>
