b90a83db42a83107c01e407f681a1b9e07fc9ac9
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-30 22:44:52 +0800

    test modal

--- resources/views/order/receipts.blade.php
+++ resources/views/order/receipts.blade.php
@@ -3,26 +3,21 @@
 @endsection
  
 @section('content')
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Open modal for @fat</button>
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
-...more buttons...
-
 <div class="modal modal-danger fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+        <h4 class="modal-title" id="exampleModalLabel">Title</h4>
       </div>
       <div class="modal-body">
         <form>
           <div class="form-group">
-            <label for="recipient-name" class="control-label">Recipient:</label>
+            <label for="recipient-name" class="control-label">Topic:</label>
             <input type="text" class="form-control" id="recipient-name">
           </div>
           <div class="form-group">
-            <label for="message-text" class="control-label">Message:</label>
+            <label for="message-text" class="control-label">Description:</label>
             <textarea class="form-control" id="message-text"></textarea>
           </div>
         </form>
@@ -218,13 +213,11 @@
 <script>
   $(document).ready(function () {
     $('#exampleModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget) // Button that triggered the modal
-      var recipient = button.data('whatever') // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this)
-      modal.find('.modal-title').text('New message to ' + recipient)
-      modal.find('.modal-body input').val(recipient)
+      var button = $(event.relatedTarget);
+      var id = button.data('id');
+      var modal = $(this);
+      modal.find('#exampleModalLabel').text(id + ' | Rejected Order Feedback');
+      modal.find('.modal-body input').val(recipient);
     });
 
     $('#order-table').DataTable({
