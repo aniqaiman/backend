1e302b803900e7604e1b2cdca2f76a205f5f9dda
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-30 22:12:22 +0800

    modal

--- resources/views/order/receipts.blade.php
+++ resources/views/order/receipts.blade.php
@@ -3,6 +3,37 @@
 @endsection
  
 @section('content')
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat">Open modal for @fat</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+...more buttons...
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">New message</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="control-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="control-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <section class="content-header">
   <h1>
@@ -79,7 +110,7 @@
                 <td class="text-nowrap">
                   <div class="btn-group-vertical btn-group-sm" role="group">
                     <button class="btn btn-success" data-id="{{ $order->id }}" data-status="1" data-type="order" onclick="updateStatus(this)">Approved</button>
-                    <button class="btn btn-danger" data-id="{{ $order->id }}" data-status="2" data-type="order" onclick="updateStatus(this)">Rejected</button>
+                    <button class="btn btn-danger" data-id="{{ $order->id }}" data-status="2" data-type="order" data-toggle="modal" data-target="#myModal">Rejected</button>
                   </div>
                 </td>
                 <td class="text-nowrap">
@@ -186,6 +217,16 @@
 @section('script')
 <script>
   $(document).ready(function () {
+    $('#exampleModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget) // Button that triggered the modal
+      var recipient = button.data('whatever') // Extract info from data-* attributes
+      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+      var modal = $(this)
+      modal.find('.modal-title').text('New message to ' + recipient)
+      modal.find('.modal-body input').val(recipient)
+    });
+
     $('#order-table').DataTable({
       'ordering': false,
     });
