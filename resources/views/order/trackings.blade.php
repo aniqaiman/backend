35e4dd30fde37d2ac3a40bd09ae712ab7a6d8259
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-04-30 23:08:55 +0800

    update minor changes

--- resources/views/order/trackings.blade.php
+++ resources/views/order/trackings.blade.php
@@ -85,9 +85,9 @@
                   </div>
                 </td>
                 <td class="text-nowrap">
-                  @if ($stock->status === 2)
-                  <button class="btn btn-success btn-sm" data-id="{{ $stock->id }}" data-status="3" data-type="order" onclick="updateStatus(this)">Completed</button>
-                  @elseif ($stock->status === 3)
+                  @if ($order->status === 2)
+                  <button class="btn btn-success btn-sm" data-id="{{ $order->id }}" data-status="3" data-type="order" onclick="updateStatus(this)">Completed</button>
+                  @elseif ($order->status === 3)
                   <button class="btn btn-success btn-sm" disabled>Completed</button>
                   @endif
                 </td>
