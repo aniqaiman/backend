79d1bd292ec34ce212dba9f2867a0b072c57d552
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-05-11 19:08:05 +0800

    add item

--- resources/views/prices/index.blade.php
+++ resources/views/prices/index.blade.php
@@ -59,7 +59,7 @@
                             <td colspan="1"><input type="number" class="selling_a" id='selling_a_{{$product["id"]}}' value='{{$product["selling_price_a"]}}'></td>
                             <td colspan="1"><input type="number" class="buying_a" id='buying_a_{{$product["id"]}}' value='{{$product["buying_price_a"]}}'></td>
                             <td colspan="1"><input type="number" class="selling_b" id='selling_b_{{$product["id"]}}' value='{{$product["selling_price_b"]}}'></td>
-                            <td colspan="1"><input type="number" class="buying_b" id="buying_b_{{$product->id}}" value="{{$product->buying_price_b}}"></td>
+                            <td colspan="1"><input type="number" class="buying_b" id='buying_b_{{$product["id"]}}' value='{{$product["buying_price_b"]}}'></td>
                             <td></td>
                             <td></td>
                             <td></td>
