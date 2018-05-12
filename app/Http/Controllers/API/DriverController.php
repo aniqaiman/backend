0632504a729c080a86f25c481b44b9ddfa38f77c
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-05-12 20:08:38 +0800

    update driver

--- app/Http/Controllers/API/DriverController.php
+++ app/Http/Controllers/API/DriverController.php
@@ -8,13 +8,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Mail;
 
-class BuyerController extends Controller
+class DriverController extends Controller
 {
-    public function postRegisterBuyer(Request $request)
+    public function postRegisterDriver(Request $request)
     {
         if (User::where('company_registration_mykad_number', $request->input('company_registration_mykad_number'))->exists()) {
             return response()->json([
-                'message' => 'The (company registration / MyKad) number had been used.',
+                'message' => 'The MyKad number had been used.',
             ], 403);
         }
 
@@ -24,76 +24,23 @@ class BuyerController extends Controller
             ], 403);
         }
 
-        $buyer = User::create([
+        $driver = User::create([
             'name' => $request->input('name'),
-            'company_name' => $request->input('company_name'),
             'company_registration_mykad_number' => $request->input('company_registration_mykad_number'),
-            'bussiness_hour' => $request->input('bussiness_hour'),
             'address' => $request->input('address'),
-            'latitude' => $request->input('latitude'),
-            'longitude' => $request->input('longitude'),
-            'mobile_number' => $request->input('mobile_number'),
             'phone_number' => $request->input('phone_number'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
-            'group_id' => 11,
-            'status_email' => 0,
-            'status_account' => 0,
+            'driver_license_number' => $request->input('driver_license_number'),
+            'driver_license_picture' => $request->input('driver_license_picture'),
+            'lorry_roadtax_expiry' => $request->input('lorry_roadtax_expiry'),
+            'lorry_type_id' => $request->input('lorry_type_id'),
+            'lorry_capacity_id' => $request->input('lorry_capacity_id'),
+            'lorry_plate_number' => $request->input('lorry_plate_number'),
+            'state_id' => 0,
+            'group_id' => 31,
         ]);
 
-        $buyer->activation_token = Crypt::encryptString($buyer->id);
-
-        Mail::send('email.registration_sucess', ['user' => $buyer], function ($message) use ($buyer) {
-
-            $message->subject('FoodRico Registration - Account Verification');
-            $message->from('wanmuz.ada@gmail.com', 'FoodRico Notification');
-            $message->to($buyer->email);
-
-        });
-
         return response()->json([
-            "data" => $buyer,
-        ]);
-    }
-
-    public function getBuyers(Request $request)
-    {
-        return response()->json([
-            "data" => User::select(
-                'id',
-                'name',
-                'company_name',
-                'company_registration_mykad_number',
-                'bussiness_hour',
-                'address',
-                'latitude',
-                'longitude',
-                'mobile_number',
-                'phone_number',
-                'email'
-            )
-                ->where('group_id', 11)
-                ->get(),
-        ]);
-    }
-
-    public function getBuyer($user_id, Request $request)
-    {
-        return response()->json([
-            "data" => User::select(
-                'id',
-                'name',
-                'company_name',
-                'company_registration_mykad_number',
-                'bussiness_hour',
-                'address',
-                'latitude',
-                'longitude',
-                'mobile_number',
-                'phone_number',
-                'email'
-            )
-                ->find($user_id),
+            "data" => $driver,
         ]);
     }
 }
