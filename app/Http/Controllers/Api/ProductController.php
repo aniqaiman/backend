c1eb440606c562897da36e5092db2731d9873240
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-05-14 14:52:41 +0800

    change to goto

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -25,14 +25,14 @@ class ProductController extends Controller
         ]);
     }
 
-    public function getFruitsByPage($page)
+    public function getFruitsByPage()
     {
         return response()->json(
             (new Product())->full(1)->paginate(30)
         );
     }
 
-    public function getVegetablesByPage($page)
+    public function getVegetablesByPage()
     {
         return response()->json(
             (new Product())->full(11)->paginate(30)
