edbbbbd4d8f69a118c6c8bcdbcbf1f7cce20c609
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-05-17 11:15:01 +0800

    responsive

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -25,123 +25,126 @@
     <div class="col-md-12">
       <div class="box box-success">
         <div class="box-body">
-          <table class="table table-bordered" id="buyer-table" style="width:100%">
-            <thead>
-              <tr class="bg-black">
-                <th>Item</th>
-                <th>SKU#</th>
-                <th>Incoming Date</th>
-                <th>Purchase Price</th>
-                <th>Total Purchased</th>
-                <th>Stock</th>
-                <th>Total Sold</th>
-                <th>Order</th>
-                <th>Remark</th>
-                <th>Wastage (E)</th>
-                <th>Promotion (E)</th>
-                <th>Edit Demand (E)</th>
-                <th style="width: 1%;"></th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($inventories as $inventory)
-              <tr>
-                <td>
-                  {{ $inventory->product->name }} (Grade {{ $inventory->grade }})
-                </td>
-                <td>{{ $inventory->product->sku }}</td>
-                <td>{{ $inventory->created_at }}</td>
-                <td>
-                  @if ($inventory->grade === 'A') {{ $inventory->product->priceValid($inventory->created_at)->seller_price_a }} @elseif ($inventory->grade
-                  === 'B') Grade B: {{ $inventory->product->priceValid($inventory->created_at)->seller_price_b }} @endif
-                </td>
-                <td>
-                  {{ $inventory->totalPurchased($inventory->product->id, $inventory->grade) }} kg
-                </td>
-                <td>
-                  <table class="table">
-                    <tr>
-                      <th>Stock#</th>
-                      <th>Supplier#</th>
-                      <th>Supplier Name</th>
-                      <th>Lorry</th>
-                      <th></th>
-                    </tr>
-                    @foreach ($inventory->stocks as $stock)
-                    <tr>
-                      <td>
-                        {{ $stock->id }}
-                      </td>
-                      <td>
-                        {{ $stock->user->id }}
-                      </td>
-                      <td>
-                        {{ $stock->user->name }}
-                      </td>
-                      <td>
-                        @if (is_null($stock->driver)) No lorry assigned @else {{ $stock->driver->name }} @endif
-                      </td>
-                      <td>
-                        {{ $stock->getQuantityByProduct($inventory->product->id, $inventory->grade) }}
-                      </td>
-                    </tr>
-                    @endforeach
-                  </table>
-                </td>
-                <td>
-                  {{ $inventory->totalSold($inventory->product->id, $inventory->grade) }} kg
-                </td>
-                <td>
-                  <table class="table">
-                    <tr>
-                      <th>Order#</th>
-                      <th>Date</th>
-                      <th>Remaining</th>
-                    </tr>
-                    @foreach ($inventory->orders as $order)
-                    <tr>
-                      <td>
-                        {{ $order->id }}
-                      </td>
-                      <td>
-                        {{ $order->created_at }}
-                      </td>
-                      <td>
-                        kg
-                      </td>
-                    </tr>
-                    @endforeach
-                  </table>
-                </td>
-                <td></td>
-                <td>
-                  <div class="input-group">
-                    <input type="number" id="wastage_{{ $inventory->product->id}}" class="wastage form-control" value="0" style="min-width: 70px;" />
-                    <div class="input-group-addon">kg</div>
-                  </div>
-                </td>
-                <td>
-                  <div class="input-group">
-                    <input type="number" class="promo form-control" promo id="promo_{{ $inventory->product->id}}" value="0" style="min-width: 70px;" />
-                    <div class="input-group-addon">kg</div>
-                  </div>
-                </td>
-                <td>
-                  <div class="input-group">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="buyer-table" style="width:100%">
+              <thead>
+                <tr class="bg-black">
+                  <th>Item</th>
+                  <th>SKU#</th>
+                  <th>Incoming Date</th>
+                  <th>Purchase Price</th>
+                  <th>Total Purchased</th>
+                  <th>Stock</th>
+                  <th>Total Sold</th>
+                  <th>Order</th>
+                  <th>Remark</th>
+                  <th>Wastage (E)</th>
+                  <th>Promotion (E)</th>
+                  <th>Edit Demand (E)</th>
+                  <th style="width: 1%;"></th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($inventories as $inventory)
+                <tr>
+                  <td>
+                    {{ $inventory->product->name }} (Grade {{ $inventory->grade }})
+                  </td>
+                  <td>{{ $inventory->product->sku }}</td>
+                  <td>{{ $inventory->created_at }}</td>
+                  <td>
+                    @if ($inventory->grade === 'A') {{ $inventory->product->priceValid($inventory->created_at)->seller_price_a }} @elseif ($inventory->grade
+                    === 'B') Grade B: {{ $inventory->product->priceValid($inventory->created_at)->seller_price_b }} @endif
+                  </td>
+                  <td>
+                    {{ $inventory->totalPurchased($inventory->product->id, $inventory->grade) }} kg
+                  </td>
+                  <td>
+                    <table class="table">
+                      <tr>
+                        <th>Stock#</th>
+                        <th>Supplier#</th>
+                        <th>Supplier Name</th>
+                        <th>Lorry</th>
+                        <th></th>
+                      </tr>
+                      @foreach ($inventory->stocks as $stock)
+                      <tr>
+                        <td>
+                          {{ $stock->id }}
+                        </td>
+                        <td>
+                          {{ $stock->user->id }}
+                        </td>
+                        <td>
+                          {{ $stock->user->name }}
+                        </td>
+                        <td>
+                          @if (is_null($stock->driver)) No lorry assigned @else {{ $stock->driver->name }} @endif
+                        </td>
+                        <td>
+                          {{ $stock->getQuantityByProduct($inventory->product->id, $inventory->grade) }}
+                        </td>
+                      </tr>
+                      @endforeach
+                    </table>
+                  </td>
+                  <td>
+                    {{ $inventory->totalSold($inventory->product->id, $inventory->grade) }} kg
+                  </td>
+                  <td>
+                    <table class="table">
+                      <tr>
+                        <th>Order#</th>
+                        <th>Date</th>
+                        <th>Remaining</th>
+                      </tr>
+                      @foreach ($inventory->orders as $order)
+                      <tr>
+                        <td>
+                          {{ $order->id }}
+                        </td>
+                        <td>
+                          {{ $order->created_at }}
+                        </td>
+                        <td>
+                          kg
+                        </td>
+                      </tr>
+                      @endforeach
+                    </table>
+                  </td>
+                  <td></td>
+                  <td>
+                    <div class="input-group">
+                      <input type="number" id="wastage_{{ $inventory->product->id}}" class="wastage form-control" value="0" style="min-width: 70px;"
+                      />
+                      <div class="input-group-addon">kg</div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="input-group">
+                      <input type="number" class="promo form-control" promo id="promo_{{ $inventory->product->id}}" value="0" style="min-width: 70px;"
+                      />
+                      <div class="input-group-addon">kg</div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="input-group">
                       @if ($inventory->grade === 'A')
                       <input type="number" class="demand form-control" data-id="{{ $inventory->product_id }}" data-grade="{{ $inventory->grade }}"
-                        value="{{ $inventory->product->demand_a }}" style="min-width: 70px;" />
-                      @elseif ($inventory->grade === 'B')
+                        value="{{ $inventory->product->demand_a }}" style="min-width: 70px;" /> @elseif
+                      ($inventory->grade === 'B')
                       <input type="number" class="demand form-control" data-id="{{ $inventory->product_id }}" data-grade="{{ $inventory->grade }}"
-                        value="{{ $inventory->product->demand_b }}" style="min-width: 70px;" />
-                      @endif
-                    <div class="input-group-addon">kg</div>
-                  </div>
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
+                        value="{{ $inventory->product->demand_b }}" style="min-width: 70px;" /> @endif
+                      <div class="input-group-addon">kg</div>
+                    </div>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
       <!-- /.box -->
@@ -252,9 +255,6 @@
           });
 
         });
-
-  
-     
 
 </script>
 @endsection
