c72aa3420487dd26860a6be51dcfe29a8d70b412
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-05-18 14:28:49 +0800

    remove unnecessary box and row

--- resources/views/orders/transactions.blade.php
+++ resources/views/orders/transactions.blade.php
@@ -220,38 +220,6 @@
           </div>
         </div>
       </div>
-      <div class="box box-success">
-        <div class="box-header">
-          <h3 class="box-title">Buyer</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse">
-              <i class="fa fa-minus"></i>
-            </button>
-          </div>
-        </div>
-        <div class="box-body">
-
-        </div>
-      </div>
-    </div>
-    <!-- /.box -->
-  </div>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="box box-success">
-        <div class="box-header">
-          <h3 class="box-title">Supplier</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse">
-              <i class="fa fa-minus"></i>
-            </button>
-          </div>
-        </div>
-        <div class="box-body">
-        </div>
-      </div>
     </div>
     <!-- /.box -->
   </div>
