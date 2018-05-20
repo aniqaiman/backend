79ff92d91be8a810d91ec925a254d2b5a087cdb9
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-05-20 08:47:49 +0800

    buuer with decimal point

--- resources/views/prices/index.blade.php
+++ resources/views/prices/index.blade.php
@@ -58,31 +58,31 @@
                                 <td colspan="1">
                                     <div class="input-group">
                                         <span class="input-group-addon">RM</span>
-                                        <input type="number" class="selling_a form-control" id='selling_a_{{ $product["id"] }}' value='{{ $product["seller_price_a"] }}' min="0.01" style="min-width: 70px">
+                                        <input type="number" class="selling_a form-control" id='selling_a_{{ $product["id"] }}' value='{{ number_format($product["seller_price_a"], 2, '.', '') }}' min="0.01" style="min-width: 70px">
                                     </div>
                                 </td>
                                 <td colspan="1">
                                     <div class="input-group">
                                         <span class="input-group-addon">RM</span>
-                                        <input type="number" class="buying_a form-control" id='buying_a_{{ $product["id"] }}' value='{{ $product["buyer_price_a"] }}' min="0.01" style="min-width: 70px">
+                                        <input type="number" class="buying_a form-control" id='buying_a_{{ $product["id"] }}' value='{{ number_format($product["buyer_price_a"], 2, '.', '') }}' min="0.01" style="min-width: 70px">
                                     </div>
                                 </td>
                                 <td colspan="1">
                                     <div class="input-group">
                                         <span class="input-group-addon">RM</span>
-                                        <input type="number" class="selling_b form-control" id='selling_b_{{ $product["id"] }}' value='{{ $product["seller_price_b"] }}' min="0.01" style="min-width: 70px">
+                                        <input type="number" class="selling_b form-control" id='selling_b_{{ $product["id"] }}' value='{{ number_format($product["seller_price_b"], 2, '.', '') }}' min="0.01" style="min-width: 70px">
                                     </div>
                                 </td>
                                 <td colspan="1">
                                     <div class="input-group">
                                         <span class="input-group-addon">RM</span>
-                                        <input type="number" class="buying_b form-control" id='buying_b_{{ $product["id"] }}' value='{{ $product["buyer_price_b"] }}' min="0.01" style="min-width: 70px">
+                                        <input type="number" class="buying_b form-control" id='buying_b_{{ $product["id"] }}' value='{{ number_format($product["buyer_price_b"], 2, '.', '') }}' min="0.01" style="min-width: 70px">
                                     </div>
                                 </td>
-                                <td>RM {{ $product["seller_yest_price_a"] }}</td>
-                                <td>RM {{ $product["buyer_yest_price_a"] }}</td>
-                                <td>RM {{ $product["seller_yest_price_b"] }}</td>
-                                <td>RM {{ $product["buyer_yest_price_b"] }}</td>
+                                <td>RM {{ number_format($product["seller_yest_price_a"], 2, '.', '') }}</td>
+                                <td>RM {{ number_format($product["buyer_yest_price_a"], 2, '.', '') }}</td>
+                                <td>RM {{ number_format($product["seller_yest_price_b"], 2, '.', '') }}</td>
+                                <td>RM {{ number_format($product["buyer_yest_price_b"], 2, '.', '') }}</td>
                                 <td>RM </td>
                                 <td></td>
                             </tr>
