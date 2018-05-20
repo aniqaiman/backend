3dfaeb86989fc9a3ccbad39750164eb7d1f315c3
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-05-20 17:25:29 +0800

    add driver

--- resources/views/orders/lorries.blade.php
+++ resources/views/orders/lorries.blade.php
@@ -77,15 +77,15 @@
     <div class="col-md-12">
       <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
-          <li class="{{ $order_active }}">
+          <li class="active">
             <a href="#tab_1" data-toggle="tab">Buyer</a>
           </li>
-          <li class="{{ $stock_active }}">
+          <li >
             <a href="#tab_2" data-toggle="tab">Supplier</a>
           </li>
         </ul>
         <div class="tab-content clearfix">
-          <div class="tab-pane {{ $order_active }}" id="tab_1">
+          <div class="tab-pane" id="tab_1">
    
           <table class="table table-bordered" id="buyer-table" style="width:100%">
             <thead>
@@ -139,6 +139,7 @@
       <!-- /.box -->
     </div>
   </div>
+</div>
 </section>
 @endsection
 
