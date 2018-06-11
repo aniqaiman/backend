1885e8fc582bdf5f276e03ac209fd462faa5d55d
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-06-11 11:43:42 +0800

    status

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -31,6 +31,8 @@ class DriverController extends Controller
         $driver->lorry_plate_number = $request->lorry_plate_number;
         $driver->location_covered = $request->location_covered;
         $driver->password = bcrypt($request->password);
+        $driver->status_email = 0;
+        $driver->status_account = 1;
         $driver->group_id = 31;
         // $driver->bank_name = $request->bank_name;
         // $driver->bank_acc_holder_name = $request->bank_acc_holder_name;
