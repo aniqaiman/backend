52819b4cf9e810499b1795fc42ec3f8150a84a54
Ibn Sani Al-Wadiyyah <ammarsdc@gmail.com>
2018-06-12 12:02:38 +0800

    use JWTAuth;

--- app/Http/Controllers/Api/SellerController.php
+++ app/Http/Controllers/Api/SellerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use JWTAuth;
 use Mail;
 
 class SellerController extends Controller
